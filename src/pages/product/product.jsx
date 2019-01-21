6ee727d30948ec41f13bef4471fdd33e16d07bfd
cyp <945139691@qq.com>
2019-01-21 16:21:41 +0800

    update

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -3,6 +3,7 @@ import {Switch,Redirect,Route} from 'react-router-dom'
 import ProductIndex from './index'
 import SaveUpdate from './save-update'
 import Detail from './detail'
+import './product.less'
 export default class Product extends Component {
   render() {
     return (
